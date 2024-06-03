4c7d86034e165afd76bc17634014826cb3bc92fb
caanrosa <carlossarmiento@uninorte.edu.co>
2024-06-02 21:05:04 -0500

    Points while driving working

--- Assets/Sign.cs
+++ Assets/Sign.cs
@@ -10,7 +10,7 @@ public class Sign : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "Player")
+        if (other.tag == "Player" || other.tag == "UsableCar")
         {
             ObjPoints.GetComponent<Points>().points += PointsReceived;
             Destroy(gameObject);
