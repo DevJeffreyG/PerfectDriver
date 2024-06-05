c5e78611c3120f7e60896dfde1a62e3130b1d64f
DevJeffreyG <gabrielpaez.r@hotmail.com>
2024-06-05 16:00:22 -0500

    Toggle Houses, Trees, y más
    Además, cambios en el render del terreno

--- Assets/Scripts/Utils/TerrainController.cs
+++ Assets/Scripts/Utils/TerrainController.cs
@@ -4,11 +4,28 @@ using UnityEngine;
 
 public class TerrainController : MonoBehaviour
 {
+    private Settings settings;
     private Terrain Terrain;
+    private GameObject houses;
     void Start()
     {
+        houses = GameObject.FindGameObjectWithTag("Houses");
+        settings = ProfileController.getProfile().getSettings();
         Terrain = GetComponent<Terrain>();
+        updateTerrain();
+    }
 
-        Terrain.treeLODBiasMultiplier = 0;
+    public void updateTerrain()
+    {
+        if ((bool) settings.getSetting(Settings.SettingName.ShowTrees))
+        {
+            Terrain.treeLODBiasMultiplier = (float)settings.getSetting(Settings.SettingName.TreesRenderDistance);
+        }
+        else
+        {
+            Terrain.treeLODBiasMultiplier = 0;
+        }
+
+        if (!(bool) settings.getSetting(Settings.SettingName.ShowHouses) && houses != null) houses.SetActive(false);
     }
 }
