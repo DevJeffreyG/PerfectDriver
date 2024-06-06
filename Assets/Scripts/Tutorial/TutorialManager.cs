711394b41217834b6a78e96085aa91a63ba1ba03
caanrosa <carlossarmiento@uninorte.edu.co>
2024-06-06 00:17:01 -0500

    all tutorial steps working

--- Assets/Scripts/Tutorial/TutorialManager.cs
+++ Assets/Scripts/Tutorial/TutorialManager.cs
@@ -1,12 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class TutorialManager : MonoBehaviour
 {
     public GameObject[] popUps;
     private int popUpIndex;
-
 
     void Update()
     {
@@ -22,22 +22,41 @@ public class TutorialManager : MonoBehaviour
             }
         }
 
-        if (popUpIndex == 0) 
+        if (popUpIndex == 0)
         {
             if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.D))
             {
                 popUpIndex++;
             }
-
-        } else if (popUpIndex == 1)
+        }
+        else if (popUpIndex == 1)
         {
-            if(Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.S)) { 
-                popUpIndex++; 
-            }
-            else if (popUpIndex == 2)
+            if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.S))
             {
-
+                popUpIndex++;
+            }
+        }
+        else if (popUpIndex == 2)
+        {
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                popUpIndex++;
+            }
+        }
+        else if (popUpIndex == 3)
+        {
+            if (Input.GetKeyDown(KeyCode.R) || Input.GetKeyDown(KeyCode.Space))
+            {
+                popUpIndex++;
+            }
+        }
+        else if (popUpIndex == 4)
+        {
+            if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.D))
+            {
+                popUpIndex++;
             }
         }
     }
-}
+ }
+
